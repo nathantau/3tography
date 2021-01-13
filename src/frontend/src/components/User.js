21ef4e207317d6ec15b5eee7405f5390a3630d25
Nathan Au <nathan.au1280@gmail.com>
2021-01-13 01:44:58 -0500

    Fixed profile picture

--- src/frontend/src/components/User.js
+++ src/frontend/src/components/User.js
@@ -17,7 +17,6 @@ const User = () => {
             }
         });
         userInfo = await userInfo.json();
-        console.log('refreshed')
         setUsername(userInfo.user);
         setImageUrls(userInfo.imageUrls);
     }
@@ -26,26 +25,24 @@ const User = () => {
         fetchUser();
     }, [])
 
-    const profileUrl = 'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
-
     return (
         <div class='container'>
             <div class='heading'>
                 <div class='profile-wrapper'>
-                    <img id='profile' src={profileUrl}/>
+                    <img id='profile' src={imageUrls[0]}/>
                 </div>
                 <h2>{username}</h2>
                 <p>UW '25 Dreamer ; Traveller; Explorer</p>
             </div>
             <div class='row'>
                 <div class='col-lg-4 col-sm-12'>
-                    <Image user='sam' url={imageUrls[0]} pos='one' refresh={fetchUser}></Image>
+                    <Image url={imageUrls[0]} pos='one' refresh={fetchUser}></Image>
                 </div>
                 <div class='col-lg-4 col-sm-12'>
-                    <Image user='sam' url={imageUrls[1]} pos='two' refresh={fetchUser}></Image>
+                    <Image url={imageUrls[1]} pos='two' refresh={fetchUser}></Image>
                 </div>
                 <div class='col-lg-4 col-sm-12'>
-                    <Image user='sam' url={imageUrls[2]} pos='three' refresh={fetchUser}></Image>
+                    <Image url={imageUrls[2]} pos='three' refresh={fetchUser}></Image>
                 </div>
             </div>
         </div>
